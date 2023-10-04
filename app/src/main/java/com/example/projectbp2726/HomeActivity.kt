6b48007b98f9f104994244f1b175b65dd8ee1574
ahmadaldiyanto <aldiahmad451@gmail.com>
2023-10-04 17:03:22 +0700

    update

--- app/src/main/java/com/example/projectbp2726/HomeActivity.kt
+++ app/src/main/java/com/example/projectbp2726/HomeActivity.kt
@@ -1,11 +1,19 @@
 package com.example.projectbp2726
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val btnBookKid:Button = findViewById(R.id.buttonMenu1)
+        btnBookKid.setOnClickListener {
+            val intent = Intent(this,BookKidActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
