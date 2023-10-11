e2e170625051d39384ab8d8a57fa577a57bded55
ahmadaldiyanto <aldiahmad451@gmail.com>
2023-10-11 17:02:52 +0700

    update

--- app/src/main/java/com/example/projectbp2726/HomeActivity.kt
+++ app/src/main/java/com/example/projectbp2726/HomeActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.util.ArrayList
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +18,26 @@ class HomeActivity : AppCompatActivity() {
             val intent = Intent(this,BookKidActivity::class.java)
             startActivity(intent)
         }
+        val btnMore:Button = findViewById(R.id.buttonMenu4)
+        btnBookKid.setOnClickListener {
+            val intent = Intent(this,AboutActivity::class.java)
+            startActivity(intent)
+        }
+        val rvBuku:RecyclerView = findViewById(R.id.recyclerViewBuku)
+        //set layout manager di recycler view
+        rvBuku.layoutManager = LinearLayoutManager(this)
+        //List data Buku
+        val data = ArrayList<BukuModel>()
+        data.add(BukuModel(R.drawable.book1,"Emi's Beach Adventure",
+            "Lorem Ipsum Dolor Sit Amet"))
+        data.add(BukuModel(R.drawable.book2, "Ade's Adventure",
+            "Lorem Ipsum Dolor Sit Amet"))
+        data.add(
+            BukuModel(R.drawable.book4, "Mermaid To Rescue",
+                "Lorem Ipsum Dolor Sit Amet"))
+        //set Adapter
+        val adapter = AdapterHome(data)
+        //set adapter ke recycler view
+        rvBuku.adapter = adapter
     }
 }
